85b4901966141608622a7b310d51dba8d70182a7
sponge-bobs-square-pants <parthchawla65@gmail.com>
2025-07-18 20:17:55 +0530

    Memory: Session summary completed, marks the end of the Memory module

--- 3. Memory/4_session_summary.py
+++ 3. Memory/4_session_summary.py
@@ -13,21 +13,20 @@ os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
 sqlite_storage = SqliteStorage(table_name="agent_sessions", db_file="../tmp/data.db")
-
-# WE CAN STORE THE USER'S DATA IN MEMORY AUTOMATICALLY AND WILL PERSISTS ACROSS ALL THE CHAT BASED ON THE USER ID
-
-# CREATING MEMORY DB
 memory_db = SqliteMemoryDb(table_name="memory", db_file="../tmp/data.db")
+
+# SESSION SUMMARY: To enable the session summary feature, we need to set enable_session_summaries to True in the Agent class.
+
 memory = Memory(db=memory_db)
 agent = Agent(
     name="General Agent",
     role="General chat agent",
     session_id="003",
     memory=memory,
-    # We need to enable the agentic memory by setting enable_agentic_memory to True
     enable_agentic_memory=True,
     storage=sqlite_storage,
     add_history_to_messages=True,
+    enable_session_summaries=True,
     read_chat_history=True,
     num_history_runs=10,
     instructions="You are a helpful assistant that always responds in a polite, upbeat and positive manner.",
@@ -38,14 +37,17 @@ agent = Agent(
     markdown=True,
     show_tool_calls=True,
 )
-
-# This is the id for the user, based upon this id the data of the user will be saved inside the sql database in the memory table.
-# You can check what data is saved using the following command:
-    # sqlite3 tmp/data.db
-    # SELECT * FROM memory WHERE user_id = 'parthchawla65@gmail.com';
-    
-
 user_id = 'parthchawla65@gmail.com'
-# agent.print_response('My name is Parth Chawla. I like to code, I am currently coding ai agents using agno.', user_id=user_id)
-agent.print_response('What are my hobbies?', user_id=user_id)
-agent.print_response('List down all the question i asked so far', user_id=user_id)
+agent.print_response('My name is Parth Chawla. I like to code, I am currently coding ai agents using agno.', user_id=user_id, session_id='003')
+agent.print_response('What are my hobbies?', user_id=user_id, session_id='003')
+agent.print_response('List down all the question i asked so far', user_id=user_id, session_id='003')
+
+# We can get the session summary using the get_session_summary method of the Memory class.
+# NOTE: The session summary takes a lot of time for each reply, as session summary is updated after each response, so it is recommended to use it only when needed.
+session_summary = memory.get_session_summary(
+        user_id=user_id, session_id='003'
+    )
+print(f"Session summary: {session_summary.summary}\n")
+
+
+
