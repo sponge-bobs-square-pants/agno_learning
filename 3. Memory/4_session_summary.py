ceb0b05c670d7faf71b09058ffc0570b70e1a315
sponge-bobs-square-pants <parthchawla65@gmail.com>
2025-07-18 20:37:51 +0530

    Updated comments for session summary

--- 3. Memory/4_session_summary.py
+++ 3. Memory/4_session_summary.py
@@ -14,10 +14,10 @@ os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
 sqlite_storage = SqliteStorage(table_name="agent_sessions", db_file="../tmp/data.db")
 memory_db = SqliteMemoryDb(table_name="memory", db_file="../tmp/data.db")
-
+memory = Memory(db=memory_db)
 # SESSION SUMMARY: To enable the session summary feature, we need to set enable_session_summaries to True in the Agent class.
 
-memory = Memory(db=memory_db)
+
 agent = Agent(
     name="General Agent",
     role="General chat agent",
@@ -38,15 +38,22 @@ agent = Agent(
     show_tool_calls=True,
 )
 user_id = 'parthchawla65@gmail.com'
+
+# Here we dont need to pass the session_id inside the method as we have defined it in our agent itself, same goes for user_id if it is defined inside the agent we dont need to pass it in every invoke.
 agent.print_response('My name is Parth Chawla. I like to code, I am currently coding ai agents using agno.', user_id=user_id, session_id='003')
-agent.print_response('What are my hobbies?', user_id=user_id, session_id='003')
-agent.print_response('List down all the question i asked so far', user_id=user_id, session_id='003')
+agent.print_response('What are my hobbies?', user_id=user_id)
+agent.print_response('My love is Masha Israfilova', user_id=user_id)
+agent.print_response('List down all the question i asked so far', user_id=user_id)
 
 # We can get the session summary using the get_session_summary method of the Memory class.
 # NOTE: The session summary takes a lot of time for each reply, as session summary is updated after each response, so it is recommended to use it only when needed.
+
+# NOTE: Session summary is generated in run time, hence it leads to slow response. It is not stored anywhere and cannot be accessed again once the agent is stopped.
 session_summary = memory.get_session_summary(
-        user_id=user_id, session_id='003'
+        user_id=user_id,
+        session_id='003'
     )
+# print(session_summary)
 print(f"Session summary: {session_summary.summary}\n")
 
 
